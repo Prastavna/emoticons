ddc87fd0f14f5071449dcebba6e441c3e7a45b0a
Pagal <ssahillppatell@gmail.com>
2022-07-28 19:20:13 +0530

    Removed Everything

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <div>Hello from Vue</div>
-</template>
-
-<style scoped>
-</style>
--- src/background.ts
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onInstalled.addListener(async () => {
-    console.log("hello");
-})
-
-// Empty export to make the file a module (To comply eith typescript)
-export {}
