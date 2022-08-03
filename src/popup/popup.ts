7ffe0e2999e2926b59851e436ae0a6266d7b4239
Pagal <ssahillppatell@gmail.com>
2022-08-03 21:23:34 +0530

    Active tab background active on selected

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -7,26 +7,35 @@ let currentTab = 'emoticons'
 // Adds navigation elements to the popup
 const navigation = document.getElementsByTagName('nav')[0]
 
-navElements.forEach((element) => {
-    const navElement = document.createElement('div')
-    const navElementImg = document.createElement('img')
-    navElementImg.src = element.icon
-    navElementImg.alt = element.name
-    navElementImg.title = element.name
+const setActiveTab = () => {
+    navigation.innerHTML = ''
+    
+    navElements.forEach((element) => {
+        const navElement = document.createElement('div')
+        const navElementImg = document.createElement('img')
+        navElementImg.src = element.icon
+        navElementImg.alt = element.name
+        navElementImg.title = element.name
+    
+        if(element.value === currentTab) {
+            navElement.classList.add('active-tab')
+        }
 
-    navElement.addEventListener('click', () => {
-        currentTab = element.value
-        updatePopup()
+        navElement.addEventListener('click', () => {
+            currentTab = element.value
+            updatePopup()
+        })
+    
+        navElement.appendChild(navElementImg)
+        navigation.appendChild(navElement)
     })
-
-    navElement.appendChild(navElementImg)
-    navigation.appendChild(navElement)
-})
+}
 
 // Updates the popup with the current tab
 const updatePopup = () => {
     const popupHead = document.querySelector('#popup .head')
 
+    setActiveTab()
     popupHead.innerHTML = `<h2>${navElements.find((element) => element.value === currentTab).name}</h2>`
 
     renderTabBody(currentTab)
